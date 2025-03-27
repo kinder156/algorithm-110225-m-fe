3ff079718d9e8e36d476a5d9ee27cd439c8f1555
alexander <alexander@telran.de>
2025-03-27 20:12:19 +0100

    Добавил функцию проверки четности

--- src/main/java/org/telran/lecture_01_intro/practice/0_sum_numbers.js
+++ src/main/java/org/telran/lecture_01_intro/practice/0_sum_numbers.js
@@ -4,3 +4,9 @@
 
 
 // Решение
+let n = 4;
+let sum = 0
+for (let i = 1 ; i<=n; i++){
+   sum += i;
+}
+console.log(sum);
--- src/main/java/org/telran/lecture_01_intro/practice/1_isEven.js
+++ src/main/java/org/telran/lecture_01_intro/practice/1_isEven.js
@@ -8,3 +8,26 @@
 
 // Решение
 
+let n = -8;
+ 
+   if (n % 2 === 0) {
+       console.log(" четное");
+   } else {
+       console.log(" нечетное");
+   }
+
+   // const_string = "hello";
+   // let reversed = "";
+   // for (let i = string.length -1;i>=0; i--){
+   //    reversed +=string[i]
+   // }
+   console.log(reversed)
+
+   const string = "hello";
+   let reversed = "";
+   for (let i = string.length -1;i>=0; i--){
+      reversed +=string[i]
+   }
+   if(string===reversed){
+      console.log()
+   }
